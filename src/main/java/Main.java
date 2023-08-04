5367b9c80c44f19af8d8db45b8b8d6243d093519
filippos.papaspyrou <filippos.papaspyrou@binance.com>
2023-08-04 13:44:53 +0300

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,75 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
+
+/**
+ * @Author: Filippos Papaspyrou
+ * @version:
+ * @Description:
+ * @Date: 04/08/2023
+ */
+public class Main {
+    public static final int SIZE = 9;
+
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        SudokuGenerator sudokuGenerator = new SudokuGenerator();
+        printBoard(sudokuGenerator.generateSudoku());
+    }
+}
+/*
+
+1 2 3    4 5 6    7 8 9
+. . .    . . .    . . .
+. . .    . . .    . . .
+
+. . .    . . .    . . .
+. . .    . . .    . . .
+. . .    . . .    . . .
+
+. . .    . . .    . . .
+. . .    . . .    . . .
+. . .    . . .    . . .
+
+
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+
+
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9
+
+
+ */
